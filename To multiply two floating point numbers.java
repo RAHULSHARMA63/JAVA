5ad396787dbcb4541fff5058f78292b85220889e
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-09 13:55:00 +0530

    To multiply two floating point numbers.

---
+++ To multiply two floating point numbers.java
@@ -0,0 +1,12 @@
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 13.44f;
+        float second = 5.62f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
