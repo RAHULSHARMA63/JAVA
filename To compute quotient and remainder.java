0137154cad8866c2ac961dd93fc8afc93f68bdab
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-11 18:32:11 +0530

    To compute quotient and remainder.

---
+++ To compute quotient and remainder.java
@@ -0,0 +1,13 @@
+public class QuotientRemainder {
+
+  public static void main(String[] args) {
+
+    int dividend = 31, divisor = 6;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
