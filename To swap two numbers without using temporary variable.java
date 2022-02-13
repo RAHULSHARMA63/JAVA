d2bf22bf6b90d31823d7d09286948c4f8f741832
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-13 12:07:37 +0530

    To swap two numbers without using temporary variable.

---
+++ To swap two numbers without using temporary variable.java
@@ -0,0 +1,19 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 52.0f, second = 33.5f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        first = first - second;
+        second = first + second;
+        first = second - first;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
