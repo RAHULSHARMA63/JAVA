ee9a4e3906b11a2cec11ae6383103381385b9916
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-22 21:49:35 +0530

    To check whether a number is positive or negative using if else.

---
+++ To check whether a number is positive or negative using if else.java
@@ -0,0 +1,13 @@
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+
+        double number = 43.6;        
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");        
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+        else
+            System.out.println(number + " is 0.");
+    }
+}
