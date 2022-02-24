2ed879c8c68afa27c7ba2c0a60f8c22977d9b737
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-24 19:23:20 +0530

    To check alphabet using ternary operator.

---
+++ To check alphabet using ternary operator.java
@@ -0,0 +1,13 @@
+public class Alphabet {
+
+    public static void main(String[] args) {
+
+        char c = 'T';
+        
+        String output = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+                ? c + " is an alphabet."
+                : c + " is not an alphabet.";
+        
+        System.out.println(output);
+    }
+}
