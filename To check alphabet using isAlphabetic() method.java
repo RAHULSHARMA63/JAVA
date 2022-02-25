4669c137eb1b1ffd3d33d9215fbe7c6472a25d64
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-02-25 18:47:48 +0530

    To check alphabet using isAlphabetic() method.

---
+++ To check alphabet using isAlphabetic() method.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    
+    char c = 'd';
+        
+    if (Character.isAlphabetic(c)) {
+      System.out.println(c + " is an alphabet.");
+    }
+    else {
+      System.out.println(c + " is not an alphabet.");
+    }
+  }
+}
