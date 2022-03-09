89e0878154436ec17e8efec48904a2544ae5dcbc
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-03-09 19:34:24 +0530

    To find GCD of two numbers using while loop and if else statement.

---
+++ To find GCD of two numbers using while loop and if else statement.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   
+    int n1 = 81, n2 = 153;    
+    while(n1 != n2) {    
+      if(n1 > n2) {
+        n1 -= n2;
+      }      
+      else {
+        n2 -= n1;
+      }
+    }
+    System.out.println("GCD: " + n1);
+  }
+}
