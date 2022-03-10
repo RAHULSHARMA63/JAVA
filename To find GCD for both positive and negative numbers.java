fa0fb02f1284f1e77bac539725fe7682d5ae72d4
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-03-10 17:49:22 +0530

    To find GCD for both positive and negative numbers.

---
+++ To find GCD for both positive and negative numbers.java
@@ -0,0 +1,18 @@
+class GCD {
+  public static void main(String[] args) {
+    int n1 = 81, n2 = -153;
+
+    n1 = ( n1 > 0) ? n1 : -n1;
+    n2 = ( n2 > 0) ? n2 : -n2;
+
+    while(n1 != n2) {        
+      if(n1 > n2) {
+        n1 -= n2;
+      }      
+      else {
+        n2 -= n1;
+      }
+    }    
+    System.out.println("GCD: " + n1);
+  }
+}
