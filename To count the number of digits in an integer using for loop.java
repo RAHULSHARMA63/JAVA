a5dc77ff585efebc62e9feae2215198e47f68432
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-03-16 22:31:19 +0530

    To count the number of digits in an integer using for loop.

---
+++ To count the number of digits in an integer using for loop.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(Sting[] args) {
+        int count = 0, num = 123456;
+        for (num != 0; num /= 10, ++count);{}
+        }
+        system.out.println("Number of digits: " + count);
+    }
+}
