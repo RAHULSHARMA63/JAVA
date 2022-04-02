89856fbe6054b2413a3cc6078afe11d703fd6a6f
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-04-02 16:59:32 +0530

    To Compute Power of Negative Number.

---
+++ To Compute Power of Negative Number.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+
+    int base = -3, exponent = 2;
+    double result = Math.pow(base, exponent);
+    System.out.println("Answer = " + result);
+  }
+}
