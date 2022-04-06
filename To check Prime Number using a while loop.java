da3e4a1019526ec1ad5c74b73a01c29dcd74b468
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-04-06 16:47:58 +0530

    To check Prime Number using a while loop.

---
+++ To check Prime Number using a while loop.java
@@ -0,0 +1,20 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 33, i = 2;
+    boolean flag = false;
+    while (i <= num / 2) {
+
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+      ++i;
+    }
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
