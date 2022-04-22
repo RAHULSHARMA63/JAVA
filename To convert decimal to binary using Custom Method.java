1e8fbe14173999d229efca8cc22df5673550378f
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-04-22 19:21:03 +0530

    To convert decimal to binary using Custom Method.

---
+++ To convert decimal to binary using Custom Method.java
@@ -0,0 +1,24 @@
+class Main {
+
+  public static void main(String[] args) {
+
+    int num = 29;
+    System.out.println("Decimal to Binary");
+    long binary = convertDecimalToBinary(num);
+    System.out.println("\n" + num + " = " + binary);
+    }
+  public static long convertDecimalToBinary(int n) {
+
+    long binaryNumber = 0;
+    int remainder, i = 1, step = 1;
+    while (n!=0) {
+      remainder = n % 2;
+        System.out.println("Step " + step++ + ": " + n + "/2");
+        System.out.println("Quotient = " + n/2 + ", Remainder = " + remainder);
+        n /= 2;
+        binaryNumber += remainder * i;
+        i *= 10;
+    }
+    return binaryNumber;
+    }
+}
