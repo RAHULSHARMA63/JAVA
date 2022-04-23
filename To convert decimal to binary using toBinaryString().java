386c5ebf3f83a5f13ed00991ac8d6c822c11496d
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-04-23 17:24:58 +0530

    To convert decimal to binary using toBinaryString().

---
+++ To convert decimal to binary using toBinaryString().java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int decimal = 65;
+    
+    String binary = Integer.toBinaryString(decimal);
+    System.out.println(decimal + " in decimal = " + binary + " in binary.");
+  }
+}
