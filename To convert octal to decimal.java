f171c36ce37fc75b2a5a412bc76ef926174fd49a
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2022-04-25 17:46:03 +0530

    To convert octal to decimal.

---
+++ To convert octal to decimal.java
@@ -0,0 +1,19 @@
+public class OctalDecimal {
+
+    public static void main(String[] args) {
+        int octal = 42;
+        int decimal = convertOctalToDecimal(octal);
+        System.out.printf("%d in octal = %d in decimal", octal, decimal);
+    }
+    public static int convertOctalToDecimal(int octal)
+    {
+        int decimalNumber = 0, i = 0;
+        while(octal != 0)
+        {
+            decimalNumber += (octal % 10) * Math.pow(8, i);
+            ++i;
+            octal/=10;
+        }
+        return decimalNumber;
+    }
+}
